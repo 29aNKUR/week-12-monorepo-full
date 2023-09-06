56bd1b5167b09e5c3c4ba12b4c66964354accb49
Ankur Semle <semleankur@gmail.com>
2023-09-06 20:37:40 +0530

    change workflows

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Use the desired Node.js version
-nvm use v20.5.1
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/week-12-monorepo-full
 git pull origin master
+yarn install
 yarn build
-/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 stop next
-/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "next" -- run "start:next"
-
-
+pm2 stop next
+pm2 start npm --name "next" -- run "start:next"
