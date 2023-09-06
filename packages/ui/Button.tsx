d4eaefcd8952a168e30194a2cff681eba63859ba
Ankur Semle <semleankur@gmail.com>
2023-09-06 14:21:48 +0530

    push

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,8 @@
 "use client";
 
 export const Button = () => {
-  return <button onClick={() => alert("boop11")}>Boop</button>;
+  return (
+    <h1>HI</h1>
+<button onClick={() => alert("boop11")}>Boop</button>;
+  )
 };
