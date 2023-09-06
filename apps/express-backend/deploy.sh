e1a7c6aba033718547789216a751fe3b4ddce704
Ankur Semle <semleankur@gmail.com>
2023-09-06 20:22:39 +0530

    deploy.sh changes

--- apps/express-backend/deploy.sh
+++ apps/express-backend/deploy.sh
@@ -3,11 +3,10 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Use the desired Node.js version
-nvm use v20.5.0
+nvm use v20.5.1
 
 cd /home/ubuntu/week-12-monorepo-full
 git pull origin master
-yarn install
 yarn build
 /home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 stop express
 /home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "express" -- run "start:express"
