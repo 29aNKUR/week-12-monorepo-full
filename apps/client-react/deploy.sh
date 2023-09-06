32d6046aa2f1fc052477edbdd75a608b2a0be80d
Ankur Semle <semleankur@gmail.com>
2023-09-06 20:15:08 +0530

    changing deploy.sh

--- apps/client-react/deploy.sh
+++ apps/client-react/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use the desired Node.js version
+nvm use v20.5.0
 
 cd /home/ubuntu/week-12-monorepo-full
 git pull origin master
 yarn install
 yarn build
-pm2 stop react
-pm2 start npm --name "react" -- run "start:react"
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 stop react
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "react" -- run "start:react"
+
+
--- apps/express-backend/deploy.sh
+++ apps/express-backend/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use the desired Node.js version
+nvm use v20.5.0
 
 cd /home/ubuntu/week-12-monorepo-full
 git pull origin master
 yarn install
 yarn build
-pm2 stop express
-pm2 start npm --name "express" -- run "start:express"
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 stop express
+/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2 start npm --name "express" -- run "start:express"
+
